4b9d5b3f3fd50683040f79f518f653fd2dda7c1c
Jeremy Bernabe <jeremy@interfused-inc.com>
2025-01-05 21:26:43 -0500

    account for relative paths

--- src/main.tsx
+++ src/main.tsx
@@ -5,9 +5,13 @@ import { BrowserRouter, Route, Routes } from "react-router";
 import App from "./App.tsx";
 import Glossary from "./pages/Glossary.tsx";
 
+// Get the first segment of the path to use as the basename
+const pathParts = window.location.pathname.split("/").filter(Boolean);
+const basename = `/${pathParts.slice(0, 2).join("/")}`;
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={basename}>
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/glossary" element={<Glossary />} />
